3f00428ec0964ee94ef88c598b3749c7dabf4c62
Nick Young <nick.yu.yang@gmail.com>
2016-04-03 00:35:57 +0800

    optimised

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use \File;
 use \Image;
-use SoapBox\Formatter\Formatter;
+use \Formatter;
 
 /**
  * 缩略图处理类
@@ -21,115 +21,116 @@ class ImageController {
     const ARGUMENT_CODE = 'InvalidArgument';
     const ARGUMENT_ERROR_MSG = 'The value: %s of parameter: %s is invalid.';
 
-    protected $path; //请求图片路径
-    protected $size; //待处理裁剪信息
-    protected $dirname; //缩略图存储目录
-    protected $filename; //缩略图名称
-    protected $filepath; //路径+名称
+    protected $image; //请求的图片
+    protected $thumb; //缩略图路径
+    private $_width = ''; //目标宽
+    private $_height = ''; //目标高
+    private $_cut = ''; //是否裁剪
+    private $_extend = ''; //是否缩放
 
     /**
      * 预处理
      * @author Nick
      *
-     * @param $key url参数
+     * @param string $key url参数
      * @return string|\Symfony\Component\HttpFoundation\BinaryFileResponse
      */
     public function process($key) {
         //处理url参数
-        $arr = explode('@', $key);
-        $this->path = $arr[0];
-        $this->size = $arr[1];
+        if (true !== $response = $this->_processParam($key))
+            return $response;
 
-        //查找请求文件
-        if (false === File::exists($this->path))
-            return $this->error(self::KEY_CODE, self::KEY_ERROR_MSG, $key);
-
-        //组织缩略图路径
-        $this->dirname = 'thumb/' . pathinfo($this->path, PATHINFO_DIRNAME);
-        $this->filename = pathinfo($this->path, PATHINFO_FILENAME) . "@$this->size";
-        $this->filepath = "$this->dirname/$this->filename.jpg";
+        //请求的图片不存在
+        if (false === File::exists($this->image))
+            return $this->_error(self::KEY_CODE, self::KEY_ERROR_MSG, $this->image);
 
         //生成缩略图
-        if (true !== $response = $this->processParam())
-            return $response;
-        elseif (File::exists($this->filepath))
-            return response()->download($this->filepath);
+        $this->_processThumb();
+
+        return response()->download($this->thumb);
     }
 
     /**
      * 处理缩略图参数
      * @author Nick
      *
+     * @param string $key
      * @return bool|mixed
      */
-    private function processParam() {
-        $width = $height = $cut = $extend = '';
-        $tmp = explode('_', $this->size);
-        foreach ($tmp as $value) {
-            if (empty($width)) {
+    private function _processParam($key) {
+        $arr = explode('@', $key);
+        if (1 == count($arr))
+            return $this->_error(self::KEY_CODE, self::KEY_ERROR_MSG, $key);
+        $this->image = $arr[0];
+        $ruleArray = explode('_', $arr[1]);
+
+        //组织缩略图路径
+        foreach ($ruleArray as $value) {
+            if (empty($this->_width)) {
                 //指定目标缩略图的宽度
                 preg_match('/.+w/', $value, $matchW);
-                !empty($matchW) && $width = rtrim($matchW[0], 'w');
+                !empty($matchW) && $this->_width = rtrim($matchW[0], 'w');
             }
-            if (empty($height)) {
+            if (empty($this->_height)) {
                 //指定目标缩略图的高度。
                 preg_match('/.+h/', $value, $matchH);
-                !empty($matchH) && $height = rtrim($matchH[0], 'h');
+                !empty($matchH) && $this->_height = rtrim($matchH[0], 'h');
             }
-            if (empty($cut)) {
+            if (empty($this->_cut)) {
                 //是否进行裁剪。如果是想对图进行自动裁剪，必须指定为1
                 preg_match('/.+c/', $value, $matchC);
-                !empty($matchC) && $cut = rtrim($matchC[0], 'c');
+                !empty($matchC) && $this->_cut = rtrim($matchC[0], 'c');
             }
-            if (empty($extend)) {
+            if (empty($this->_extend)) {
                 //缩放优先边，这里指定按短边优化
                 preg_match('/.+e/', $value, $matchE);
-                !empty($matchE) && $extend = rtrim($matchE[0], 'e');
+                !empty($matchE) && $this->_extend = rtrim($matchE[0], 'e');
             }
         }
 
-        if (!is_numeric($width))
-            return $this->error(self::ARGUMENT_CODE, vsprintf(self::ARGUMENT_ERROR_MSG, [$width, 'w']));
-        elseif (!is_numeric($height))
-            return $this->error(self::ARGUMENT_CODE, vsprintf(self::ARGUMENT_ERROR_MSG, [$height, 'h']));
-        elseif (!in_array($cut, ['0', '1']))
-            return $this->error(self::ARGUMENT_CODE, vsprintf(self::ARGUMENT_ERROR_MSG, [$cut, 'c']));
-        elseif (!in_array($extend, ['0', '1']))
-            return $this->error(self::ARGUMENT_CODE, vsprintf(self::ARGUMENT_ERROR_MSG, [$extend, 'e']));
+        if (!is_numeric($this->_width))
+            return $this->_error(self::ARGUMENT_CODE, vsprintf(self::ARGUMENT_ERROR_MSG, [$this->_width, 'w']));
+        elseif (!is_numeric($this->_height))
+            return $this->_error(self::ARGUMENT_CODE, vsprintf(self::ARGUMENT_ERROR_MSG, [$this->_height, 'h']));
+        elseif (!in_array($this->_cut, ['0', '1']))
+            return $this->_error(self::ARGUMENT_CODE, vsprintf(self::ARGUMENT_ERROR_MSG, [$this->_cut, 'c']));
+        elseif (!in_array($this->_extend, ['0', '1']))
+            return $this->_error(self::ARGUMENT_CODE, vsprintf(self::ARGUMENT_ERROR_MSG, [$this->_extend, 'e']));
         else
-            $this->processThumb($width, $height, $cut, $extend);
+        {
+            $rule = "{$this->_width}w_{$this->_height}h_{$this->_cut}c_{$this->_extend}e";
+            $pathinfo = pathinfo($this->image);
+            $this->thumb = "thumb/{$pathinfo['dirname']}/{$pathinfo['filename']}@{$rule}.jpg";
+        }
         return true;
     }
 
     /**
      * 处理缩略图
      *
-     * @param $width
-     * @param $height
-     * @param $cut
-     * @param $extend
      */
-    private function processThumb($width, $height, $cut, $extend) {
+    private function _processThumb() {
         //读取图片
-        $image = Image::make($this->path);
+        $image = Image::make($this->image);
         $realW = $image->getWidth(); //实际宽度
         $realH = $image->getHeight(); //实际高度
 
         //缩放
-        if (1 == $extend) { //按短边缩放
-            $realW < $realH ? $image->widen($width) : $image->heighten($height);
+        if (1 == $this->_extend) { //按短边缩放
+            $realW < $realH ? $image->widen($this->_width) : $image->heighten($this->_height);
         }
         else { //按长边缩放
-            $realW < $realH ? $image->heighten($height) : $image->widen($width);
+            $realW < $realH ? $image->heighten($this->_height) : $image->widen($this->_width);
         }
         //裁剪，非短边缩放不裁剪
-        if (1 == $cut && 1 == $extend) {
-            $image->fit($width, $height);
+        if (1 == $this->_cut && 1 == $this->_extend) {
+            $image->fit($this->_width, $this->_height);
         }
 
         //保存图片
-        File::isDirectory($this->dirname) || File::makeDirectory($this->dirname, 0755, true, true);
-        $image->save($this->filepath);
+        $dir = pathinfo($this->thumb, PATHINFO_DIRNAME);
+        File::isDirectory($dir) || File::makeDirectory($dir, 0755, true, true);
+        $image->save($this->thumb);
     }
 
     /**
@@ -141,7 +142,7 @@ class ImageController {
      * @param $key
      * @return mixed
      */
-    private function error($code, $message, $key = '') {
+    private function _error($code, $message, $key = '') {
         $content['Error']['Code'] = $code;
         $content['Error']['Message'] = $message;
         !empty($key) && $content['Error']['Key'] = $key;
